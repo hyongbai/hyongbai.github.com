e463070a26c7e2b43c9038ec8de6cad11c2cef83
hyongbai <hyongbai@gmail.com>
2017-09-26 10:25:07 +0800

    [blog] shell for lauch through docker

--- launch.sh
+++ launch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-rm -rf _site
-open "http://127.0.0.1:4000"
-jekyll s
+
+# config
+_blog_c_name="blog.yourbay.me"
+_blog_work_dir=$(cd "$(dirname "$0")"; pwd)
+cid=`docker ps -a | grep "${_blog_c_name}" | awk '{print $1}'`
+
+# start docker container
+[[ "${cid}" ]] && docker start ${cid}
+[[ ! "${cid}" ]] && docker run --name ${_blog_c_name} -p 4000:4000 -v \
+"${_blog_work_dir}":/srv -w /srv -d jekyll/jekyll:3.5.2 \
+/bin/sh -c "jekyll clean && jekyll build && jekyll serve"
